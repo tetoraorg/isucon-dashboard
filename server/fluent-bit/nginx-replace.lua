dd7dea0fc0a7f232c118e91e8e9d9f34d1de04cc
Ras96 <asymptote.k.k@gmail.com>
2022-07-21 09:33:39 +0900

    :zap: parseをserver側でやるようにした

---
+++ server/fluent-bit/nginx-replace.lua
@@ -0,0 +1,9 @@
+function cb_replace(tag, timestamp, record)
+  local path = record["path"]
+  path = string.gsub(path, '=[^&=]+', "=Q")
+  path = string.gsub(path, '/%x%x%x%x%x%x%x%x%-%x%x%x%x%-%x%x%x%x%-%x%x%x%x%-%x%x%x%x%x%x%x%x%x%x%x%x', "/UUID")
+  path = string.gsub(path, '/%d+', "/ID")
+  record["path"] = path
+
+  return 2, timestamp, record
+end
