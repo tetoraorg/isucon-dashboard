e7848a01a673501fd8335721112ad4f3c08ee995
Ras96 <asymptote.k.k@gmail.com>
2022-07-23 07:59:17 +0900

    :bug: fix error hangling

--- server/fluent-bit/nginx-replace.lua
+++ server/fluent-bit/nginx-replace.lua
@@ -1,4 +1,9 @@
 function cb_replace(tag, timestamp, record)
+  -- discard not-parsed records
+  if record["path"] == nil then
+    return -1, 0, 0
+  end
+
   local path = record["path"]
   path = string.gsub(path, '=[^&=]+', "=Q")
   path = string.gsub(path, '/%x%x%x%x%x%x%x%x%-%x%x%x%x%-%x%x%x%x%-%x%x%x%x%-%x%x%x%x%x%x%x%x%x%x%x%x', "/UUID")
